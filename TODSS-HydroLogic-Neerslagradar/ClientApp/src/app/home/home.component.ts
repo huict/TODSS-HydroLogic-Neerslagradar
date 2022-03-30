9b911421dc9535a632937948ea5f2a99d5ca5320
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-03-30 17:45:54 +0200

    Added home component

--- TODSS-HydroLogic-Neerslagradar/ClientApp/src/app/home/home.component.ts
+++ TODSS-HydroLogic-Neerslagradar/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  public views_test: number[] = [];
+
+  constructor() { }
+
+  public addViewTest() {
+    let last;
+    if (this.views_test.length == 0) {
+      last = -1;
+    } else {
+      last = this.views_test[this.views_test.length-1]
+    }
+    this.views_test.push(last+1)
+  }
 }
