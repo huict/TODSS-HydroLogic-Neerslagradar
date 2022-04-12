65d25b8243a0a6a25aeebb44ac97525a8eb1d988
Dyllanus <dyllan.salverius15@gmail.com>
2022-04-12 14:26:09 +0200

    nieuwe method aangemaakt

--- TODSS-HydroLogic-Neerslagradar/ServerApp/Application/IRadarImageService.cs
+++ TODSS-HydroLogic-Neerslagradar/ServerApp/Application/IRadarImageService.cs
@@ -1,10 +1,12 @@
 ﻿using System.Drawing;
 using Microsoft.AspNetCore.Mvc;
+using TODSS_HydroLogic_Neerslagradar.ServerApp.Presentation.DTO;
 
 namespace TODSS_HydroLogic_Neerslagradar.ServerApp.Application;
 
 public interface IRadarImageService
 {
-    List<byte[]> loadData();
-    
+    IEnumerable<byte[]> loadData();
+    IEnumerable<byte[]> GetSpecificSlices(WeatherFiltersDTO dto);
+
 }
--- TODSS-HydroLogic-Neerslagradar/ServerApp/Application/RadarImageService.cs
+++ TODSS-HydroLogic-Neerslagradar/ServerApp/Application/RadarImageService.cs
@@ -1,22 +1,27 @@
 ﻿using System.Drawing;
 using System.Drawing.Imaging;
-using Microsoft.AspNetCore.Mvc;
 using Rewrite_NetCdf_test.classes.colorSchemes;
 using TODSS_HydroLogic_Neerslagradar.ServerApp.Domain;
 using TODSS_HydroLogic_Neerslagradar.ServerApp.Domain.ColorSchemes;
 using TODSS_HydroLogic_Neerslagradar.ServerApp.Domain.Reading_Data;
+using TODSS_HydroLogic_Neerslagradar.ServerApp.Presentation.DTO;
 
 namespace TODSS_HydroLogic_Neerslagradar.ServerApp.Application;
 
 public class RadarImageService : IRadarImageService
 {
-    public List<byte[]> loadData()
+    public IEnumerable<byte[]> loadData()
     {
         var ds = new ReadingData(@"C:\Users\salni\RiderProjects\TODSS-HydroLogic-Neerslagradar\TODSS-HydroLogic-Neerslagradar\sources\Knmi.Radar.Uncorrected_20210618_original.nc");
         var scheme = new PreciezeColor();
         var bitmaps = GeneratePhoto.GenerateBitmap(ds.GetSlices(0, 0, 1, 700, 765, 36), scheme);
         iets(bitmaps[0]);
         return bitmaps;
+    }
+
+    public IEnumerable<byte[]> GetSpecificSlices(WeatherFiltersDTO dto)
+    {
+        throw new NotImplementedException();
     }
 
     private void iets(byte[] array)
