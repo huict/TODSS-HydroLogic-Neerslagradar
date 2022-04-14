68a26cf15974e3908679092bab454f98d27b751f
arvid <arvid.smit@xs4all.nl>
2022-04-14 12:50:27 +0200

    Added if to decide if you wanna start front-end

--- TODSS-HydroLogic-Neerslagradar/Program.cs
+++ TODSS-HydroLogic-Neerslagradar/Program.cs
@@ -1,35 +1,37 @@
 using TODSS_HydroLogic_Neerslagradar.ServerApp.Application;
 
-var builder = WebApplication.CreateBuilder(args);
+var startWebsite = true;
+
+if (startWebsite)
+{
+    var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddScoped<IRadarImageService, RadarImageService>();
+    builder.Services.AddControllersWithViews();
 
-builder.Services.AddControllers();
+    builder.Services.AddScoped<IRadarImageService, RadarImageService>();
 
-var app = builder.Build();
+    var app = builder.Build();
+    builder.Services.AddControllers();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    if (!app.Environment.IsDevelopment())
+    {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
+
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller}/{action=Index}/{id?}");
+
+    app.MapFallbackToFile("index.html");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html");
-
-//
-// var gdal = new GDALProvider();
-//
-// gdal.loadData();
-
-app.Run();
