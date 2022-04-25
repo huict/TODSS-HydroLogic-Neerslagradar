4c6f1d7daa16a21940aba2dd82986b7a2d3b4f04
sendrastisch <sandra.fernandes@student.hu.nl>
2022-04-25 02:06:32 +0200

    add scroll back to top button

--- TODSS-HydroLogic-Neerslagradar/ClientApp/src/app/configuration-select/configuration-select.component.ts
+++ TODSS-HydroLogic-Neerslagradar/ClientApp/src/app/configuration-select/configuration-select.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, HostListener, Inject, OnInit, ViewChild} from '@angular/core';
 import {ConfigurationManager} from "./configuration.manager";
 import {IConfiguration} from "../home/home.component";
+import {DOCUMENT} from "@angular/common";
 
 /**
  * A selection component that gives a brief summary of each saved configuration that can be selected or removed.
@@ -12,15 +13,30 @@ import {IConfiguration} from "../home/home.component";
   styleUrls: ['./configuration-select.component.css']
 })
 export class ConfigurationSelectComponent implements OnInit {
+  showMe:boolean=false
 
-  constructor(public manager:ConfigurationManager) { }
+  constructor(
+    public manager:ConfigurationManager,
+  ) { }
 
   ngOnInit(): void {
   }
 
   getAllConfigs() : IConfiguration[]{
-    console.log(Object.values(this.manager.getAllConfigs()))
     return Object.values(this.manager.getAllConfigs())
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    this.showMe = window.scrollY != 0;
+  }
+
+  scrollTop(){
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
   }
 
 }
