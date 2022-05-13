ac4a250a82d83bd6decfbf8aa7158d868763dc50
arvid <arvid.smit@xs4all.nl>
2022-05-13 15:15:52 +0200

    Grid 0,0 is now at top left instead of bottom left

--- TODSS-HydroLogic-Neerslagradar/ServerApp/Domain/CoordinateConversion/CoordinateConversion.cs
+++ TODSS-HydroLogic-Neerslagradar/ServerApp/Domain/CoordinateConversion/CoordinateConversion.cs
@@ -48,7 +48,7 @@ public class CoordinateConversion
             for (int x = 0; x < columnAmount + 1; x++)
             {
                 pointmap[x, y] = PointCoordinatesConversion(new[]
-                    {XLowerLeft + x * cellWidth, YLowerLeft + y * cellHeight});
+                    {XLowerLeft + x * cellWidth, YLowerLeft + rowAmount * cellHeight - y * cellHeight});
             }
         }
 
