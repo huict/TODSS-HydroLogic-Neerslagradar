99b3ee24c36564a196454e4443c523975b006f81
Dyllanus <74240279+Dyllanus@users.noreply.github.com>
2022-05-17 13:37:44 +0200

    - GeoDataDTO.cs is now a struct - added comments for GenerateGeoJSON.cs

--- TODSS-HydroLogic-Neerslagradar/ServerApp/Presentation/DTO/GeoDataDTO.cs
+++ TODSS-HydroLogic-Neerslagradar/ServerApp/Presentation/DTO/GeoDataDTO.cs
@@ -2,7 +2,7 @@
 
 namespace TODSS_HydroLogic_Neerslagradar.ServerApp.Presentation.DTO;
 
-public class GeoDataDTO
+public struct GeoDataDTO
 {
     public List<List<List<double>>> coords { get; set; }
     public float intensity { get; set; }
