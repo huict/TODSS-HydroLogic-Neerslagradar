441e86c01e72c1a6802679555ae766fd199d4524
ArviWasTaken <arvid.smit@xs4all.nl>
2022-05-17 13:37:37 +0200

    started on

---
+++ TODSS-HydroLogic-Neerslagradar/ServerApp/Application/IStatistiekenService.cs
@@ -0,0 +1,9 @@
+﻿namespace TODSS_HydroLogic_Neerslagradar.ServerApp.Application;
+
+public interface IStatistiekenService
+{
+    int CalculateSum();
+    double CalculateAverageRainFallPerCellInSelectionForPeriod(long startTime, long endTime);
+    double CalculateAverageRainFallInCellForPeriod(int x, int y, long startTime, long endTime);
+    
+}
---
+++ TODSS-HydroLogic-Neerslagradar/ServerApp/Presentation/StatistiekenController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TODSS_HydroLogic_Neerslagradar.ServerApp.Presentation;
+
+[ApiController]
+[Route("[Controller]")]
+public class StatistiekenController : ControllerBase
+{
+    
+}
