21f7a1fadf19207887d7553b8b9a7364cd3d1889
Dyllanus <dyllan.salverius15@gmail.com>
2022-05-24 11:05:21 +0200

    - new Dto so the user can specify the comperssion

---
+++ TODSS-HydroLogic-Neerslagradar/ServerApp/Presentation/DTO/GridCellsInDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace TODSS_HydroLogic_Neerslagradar.ServerApp.Presentation.DTO;
+
+public class InGridCellsDTO
+{
+    public int CombineFields { get; set; }
+    public bool LargeDataset { get; set; }
+}
