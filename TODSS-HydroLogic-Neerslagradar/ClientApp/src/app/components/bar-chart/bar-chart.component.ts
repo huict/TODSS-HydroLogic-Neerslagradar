7051734b01a3e1db631d041b7b8b674e1b33a4f5
evaolthofhu <eva.olthof@student.hu.nl>
2022-06-06 14:02:27 +0200

    initial commit for post request graph data

--- TODSS-HydroLogic-Neerslagradar/ClientApp/src/app/components/bar-chart/bar-chart.component.ts
+++ TODSS-HydroLogic-Neerslagradar/ClientApp/src/app/components/bar-chart/bar-chart.component.ts
@@ -1,18 +1,50 @@
-﻿import {Component, Input} from '@angular/core';
+﻿import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import { ChartOptions, ChartType, ChartDataset } from 'chart.js';
 import {ICoordinateFilter, ITimeFilter} from "../../templates/i-weather.template";
+import {HttpClient} from "@angular/common/http";
+
 // import { Label } from 'ng2-charts';
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.css']
 })
-export class BarChartComponent {
+export class BarChartComponent implements OnInit {
   private _coordinatesFilter: ICoordinateFilter | undefined;
   private _timeFilter: ITimeFilter | undefined;
 
-  constructor() {
+  constructor(private http: HttpClient) {
+
   }
+
+  ngOnInit() {
+    this.updateGraph();
+  }
+
+  @ViewChild("chart", {static: true}) chart!: ElementRef;
+
+  private updateGraph() {
+    this.http.post("https://localhost:7187/radarimage/intensity", `{
+      "TopLeftLongitude":3.96395,
+      "TopLeftLatitude":52.98227,
+      "TopRightLongitude":4.13571,
+      "TopRightLatitude":52.97493,
+      "BotRightLongitude":4.12333,
+      "BotRightLatitude":52.87131,
+      "BotLeftLongitude":3.95208,
+      "BotLeftLatitude":52.87862,
+      "StartSeconds":0,
+      "EndSeconds":8000,
+      "CombineFields":1
+    }`, {headers: {"Content-Type": "application/json"}}).subscribe(e => {
+      let data = e as RequestData[][];
+      let processData = data.map(val => {
+        return val.reduce((total, intens) => intens.intensity, 0);
+      })
+      this.chart.nativeElement.barChartData = [{ data: processData}];
+    })
+  }
+
 
   barChartOptions: ChartOptions = {
     responsive: true,
@@ -24,10 +56,11 @@ export class BarChartComponent {
   barChartPlugins = [];
   barChartData: ChartDataset[] = [
     {
-      data: [4.5, 3.7, 6.0, 7.0, 4.6, 3.3],
+      data: [],
       label: 'Hoeveelheid neerslag in mm'
     }
   ];
+
 
   @Input() set coordinatesFilter(value: ICoordinateFilter | undefined) {
     console.log(value)
@@ -40,4 +73,9 @@ export class BarChartComponent {
     this._timeFilter = value;
   }
 
+}
+
+interface RequestData {
+  id: number;
+  intensity: number;
 }
