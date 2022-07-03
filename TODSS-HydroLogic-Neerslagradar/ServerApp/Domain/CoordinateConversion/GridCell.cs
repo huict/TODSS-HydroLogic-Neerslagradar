c43ff8bbdd940fb6fecb1c03bf7a714e6fbafd7f
arvid <arvid.smit@xs4all.nl>
2022-07-03 21:44:12 +0200

    Refactor CoordinateConversion: Removed CoordForGeoJson

--- TODSS-HydroLogic-Neerslagradar/ServerApp/Domain/CoordinateConversion/GridCell.cs
+++ TODSS-HydroLogic-Neerslagradar/ServerApp/Domain/CoordinateConversion/GridCell.cs
@@ -2,21 +2,22 @@
 
 namespace TODSS_HydroLogic_Neerslagradar.ServerApp.Domain.CoordinateConversion;
 
+/// <summary>
+/// Connects the calculated coordinates to the corresponding X and Y of a cell in the data
+/// </summary>
 public class GridCell
 {   
     public double[] Coordinates;
     public int X;
     public int Y;
-    public List<List<List<double>>> coordsForGeoJson;
-    
+
     public GridCell(double[] coordinates, int x, int y)
     {
         Coordinates = coordinates;
         X = x;
         Y = y;
-        coordsForGeoJson = GenerateCoordsGeoJson(coordinates);
     }
-
+    
     public static List<List<List<double>>> GenerateCoordsGeoJson(IReadOnlyList<double> coordinates)
     {
         var list2 = new List<List<double>>();
@@ -28,8 +29,6 @@ public class GridCell
             };
             list2.Add(list);
         }
-        // list2.Add(list2[0]);
         return new List<List<List<double>>>{list2};
-        
     }
 }
