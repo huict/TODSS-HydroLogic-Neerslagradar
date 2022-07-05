32a5986e820e1b9e4027b27fc7716666ea453896
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-07-05 13:45:48 +0200

    Small changes to bar chart

--- TODSS-HydroLogic-Neerslagradar/ClientApp/src/app/components/bar-chart/bar-chart.component.ts
+++ TODSS-HydroLogic-Neerslagradar/ClientApp/src/app/components/bar-chart/bar-chart.component.ts
@@ -1,15 +1,14 @@
-﻿import {Component, Input, OnInit} from '@angular/core';
+﻿import {Component, Input} from '@angular/core';
 import {ChartOptions, ChartType, ChartDataset} from 'chart.js';
 import {ICoordinateFilter, ITimeFilter} from "../../templates/i-weather.template";
 import {HttpClient} from "@angular/common/http";
 
-// import { Label } from 'ng2-charts';
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.css']
 })
-export class BarChartComponent implements OnInit {
+export class BarChartComponent {
   private _coordinatesFilter: ICoordinateFilter | undefined;
   private _timeFilter: ITimeFilter | undefined;
 
@@ -26,14 +25,10 @@ export class BarChartComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-
-  }
-
   private updateGraph() {
     let cf = this._coordinatesFilter;
     if (cf == undefined) return;
-    if (cf.pixels.length === 0) {return;}
+    if (cf.pixels.length === 0) return;
     let begin:number = <number>this._timeFilter?.beginTimestamp;
     let end:number = <number>this._timeFilter?.endTimestamp;
     let body = {
@@ -45,13 +40,10 @@ export class BarChartComponent implements OnInit {
 
     this.http.post("https://localhost:7187/graph", body, {headers: {"Content-Type": "application/json"}}).subscribe(e => {
       let data = e as RequestData[];
-      console.log(data)
 
       // @ts-ignore
       this.barChartLabels = data.map((_, index) => this._getDateLabelFromIndex(index));
       this.barChartData.pop();
-      this.barChartData.pop();
-      // this.barChartData.push({label:"Totale hoeveelheid neerslag in mm", data: data.map(v => v.cumulative)});
       this.barChartData.push({label:"Gemiddelde hoeveelheid neerslag in mm", data: data.map(v => v.average)});
     })
   }
@@ -59,8 +51,7 @@ export class BarChartComponent implements OnInit {
   private _getDateLabelFromIndex(i: number):string {
     let add0Func = (number:number) => {
       let strnum: string = String(number)
-      if (strnum.length == 1) return "0" + strnum
-      return strnum
+      return strnum.length == 1 ? "0" + strnum : strnum
     };
     let begin:number = <number>this._timeFilter?.beginTimestamp;
     let d = new Date(begin + i*300000);
